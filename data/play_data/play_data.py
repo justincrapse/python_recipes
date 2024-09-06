b6ca66392cff3fa55b6b01878be7ca65f08a5a6e
justincrapse <justincrapse@gmail.com>
2024-09-06 15:45:33 -0700

    first c

---
+++ data/play_data/play_data.py
@@ -0,0 +1,4 @@
+from random import random
+
+def get_list_of_floats(item_count):
+    return [random(i) for i in range(item_count)]
---
+++ python_foundations/map_filter_reduce/map_examples.py
@@ -0,0 +1,5 @@
+from data.play_data import get_list_of_floats
+
+float_list_50 = get_list_of_floats(50)
+
+print(float_list_50)
